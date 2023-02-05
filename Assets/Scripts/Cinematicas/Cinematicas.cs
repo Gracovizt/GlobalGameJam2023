13cf61697844284804a138c58ac0e8af86d67b4b
Graco <61621247+Gracovizt@users.noreply.github.com>
2023-02-05 06:56:05 +0100

    Espada a la verga hermano

--- Assets/Scripts/Cinematicas/Cinematicas.cs
+++ Assets/Scripts/Cinematicas/Cinematicas.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Cinematicas : MonoBehaviour
 {
     public GameObject player;
-
-    
+    public Animator anim;
+    public Animator fafafade;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            anim.SetTrigger("Espada");
+            StartCoroutine(Fafafafafafade());
             player.gameObject.GetComponent<PlayerMovement>().enabled = false;
             player.gameObject.GetComponent<PlayerAttack>().enabled = false;
         }
+    }
+
+    IEnumerator Fafafafafafade()
+    {
+        yield return new WaitForSeconds(2f);
+        fafafade.SetTrigger("Black");
     }
 }
